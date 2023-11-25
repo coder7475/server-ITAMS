4ece126b8fb1a547fad5da91ed79d16a0f75b86a
coder7475 <robiulhossain7475@gmail.com>
2023-11-25 19:48:16 +0600

    feat: add middleware necessary for express app

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.xjslrno.mongodb.net/?retryWrites=true&w=majority`
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const port = process.env.PORT || 5000
 
@@ -19,12 +25,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const myDB = client.db('assetsIT')
+    const userCollection = myDB.collection("users")
+
+    // users related apis
+    // ? get all users in the database
+    app.get('/api/v1/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    // ? create user in users collection if it does not already exits
+    app.post('/api/v1/users', async (req, res) => {
+      const user = req.body;
+      // const query = { email: user.email }
+      // const existingUser = await userCollection.findOne(query)
+
+      // if (existingUser) {
+      //   return res.send({ message: 'user already exists', insertedId: null })
+      // }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
