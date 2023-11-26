addbbde358e0a8548d55e9ea708650a851020623
coder7475 <robiulhossain7475@gmail.com>
2023-11-26 23:26:37 +0600

    feat: create an api to save payment history

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.xjslrno.mongodb.net/?retryWrites=true&w=majority`
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.xjslrno.mongodb.net/?retryWrites=true&w=majority`;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // middleware
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,32 +19,33 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const myDB = client.db('assetsIT')
-    const userCollection = myDB.collection("users")
+    const myDB = client.db("assetsIT");
+    const userCollection = myDB.collection("users");
+    const paymentCollection = myDB.collection("payments");
 
     // users related apis
     // ? get all users in the database
-    app.get('/api/v1/users', async (req, res) => {
+    app.get("/api/v1/users", async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
 
     // insert user into database
     // ? create user in users collection if it does not already exits
-    app.post('/api/v1/users', async (req, res) => {
+    app.post("/api/v1/users", async (req, res) => {
       const user = req.body;
-      const query = { email: user.email }
-      const existingUser = await userCollection.findOne(query)
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
 
       if (existingUser) {
-        return res.send({ message: 'user already exists', insertedId: null })
+        return res.send({ message: "user already exists", insertedId: null });
       }
 
       const result = await userCollection.insertOne(user);
@@ -52,40 +53,64 @@ async function run() {
     });
 
     // check if user is admin
-    app.get('/api/v1/users/admin/:email', async(req, res) => {
+    //? get admins details
+    app.get("/api/v1/users/admin/:email", async (req, res) => {
       const email = req.params.email;
 
       const query = { email: email };
       const user = await userCollection.findOne(query);
       let admin = false;
-
-      if (user){
-        admin = user?.role === 'admin';
+      // console.log(user);
+      if (user) {
+        admin = user?.role === "admin";
       }
 
-      res.send({ admin })
+      res.send({ admin, user });
+    });
 
-    })
+    // update the admin package payment from unpaid to paid
+    app.patch("/api/v1/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const data = req.body;
+      const query = { email: email };
+      const updateDocument = {
+        $set: {
+          package: data,
+        },
+      };
+
+      const result = await userCollection.updateOne(query, updateDocument);
+      res.send(result);
+    });
 
     // ? Payment Intent
-    app.post("/api/v1/create-payment-intent", async(req, res) => {
+    app.post("/api/v1/create-payment-intent", async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100);
 
       const paymentIntent = await stripe.paymentIntents.create({
         amount,
         currency: "usd",
-        payment_method_types: ['card']
+        payment_method_types: ["card"],
       });
-    
+
       res.send({
         clientSecret: paymentIntent.client_secret,
       });
+    });
+
+    // saved the payment history
+    app.post("/api/v1/payments", async(req, res) => {
+      const payment = req.body;
+      const result = await paymentCollection.insertOne(payment);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -93,11 +118,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello ITAM!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello ITAM!");
+});
 
 app.listen(port, () => {
-  console.log(`ITAM Express app listening on port ${port}`)
-})
+  console.log(`ITAM Express app listening on port ${port}`);
+});
