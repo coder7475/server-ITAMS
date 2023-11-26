c69873da35a548e657061ab5c2815afdf78e4648
coder7475 <robiulhossain7475@gmail.com>
2023-11-26 14:10:53 +0600

    feat: add an api to check if user is admin

--- index.js
+++ index.js
@@ -35,20 +35,36 @@ async function run() {
       res.send(result);
     });
 
+    // insert user into database
     // ? create user in users collection if it does not already exits
     app.post('/api/v1/users', async (req, res) => {
       const user = req.body;
-      // const query = { email: user.email }
-      // const existingUser = await userCollection.findOne(query)
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
 
-      // if (existingUser) {
-      //   return res.send({ message: 'user already exists', insertedId: null })
-      // }
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
 
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
+    // check if user is admin
+    app.get('/users/admin/:email', async(req, res) => {
+      const email = req.params.email;
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+
+      if (user){
+        admin = user?.role === 'admin';
+      }
+
+      res.send({ admin })
+
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
