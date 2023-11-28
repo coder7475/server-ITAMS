e7c623de62c7eb5024b254d665ec1d75e8ad0c5b
coder7475 <robiulhossain7475@gmail.com>
2023-11-28 12:14:58 +0600

    feat: create apis to add member to the team and remove the tem

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -121,6 +121,32 @@ async function run() {
     app.post("/api/v1/user/makeCustomRequest", async(req, res) => {
       const custRequest = req.body;
       const result = await requestCollection.insertOne(custRequest);
+      res.send(result);
+    })
+
+    // add a user to the team via admin
+    app.put("/api/v1/admin/addToTeam/:id", async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const data = req.body;
+      const updatedDoc = {
+        $set: data
+      }
+      // console.log(data);
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    // remove a user to the team via admin
+    app.put("/api/v1/admin/removeFromTeam/:id", async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const data = req.body;
+      const updatedDoc = {
+        $unset: data
+      }
+      // console.log(data);
+      const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
     })
 
